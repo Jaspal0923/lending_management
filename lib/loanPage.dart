d71c3c898ce4cc5914bdd0b610c63b1e6fc931a4
Jaspal Singh <jaspalsingh@Jaspals-MacBook-Air.local>
2023-12-08 17:21:42 +0800

    First Commit

---
+++ lib/loanPage.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:lending_management/text_field.dart';
+
+class LoanPage extends StatefulWidget {
+  const LoanPage({super.key});
+
+  @override
+  State<LoanPage> createState() => _LoanPageState();
+}
+
+class _LoanPageState extends State<LoanPage> {
+  final TextEditingController _userController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void dispose(){
+    super.dispose();
+    _userController.dispose();
+    _passwordController.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container (
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+      
+          //Login Forms
+          children: [
+            //add flexible to create spacing
+            Flexible(flex: 2,child: Container(),),
+            const Text(
+              'LOGIN',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+            const SizedBox(
+              //Sa SizedBox ang e butang mga fields
+              height: 35,
+            ),
+            //UserName Field
+            TextField_LogIn(
+              textEditingController: _userController, 
+              hintText: 'Enter Username', 
+              textInputType:TextInputType.text 
+            ),
+            
+            const SizedBox(//SPACING
+              //Sa SizedBox ang e butang mga fields
+              height: 20,
+            ),
+            //Password Field
+            TextField_LogIn(
+              textEditingController: _passwordController, 
+              hintText: 'Enter Password', 
+              textInputType: TextInputType.text,
+              isPass: true,
+            ),
+            
+            const SizedBox(//SPACING
+              //Sa SizedBox ang e butang mga fields
+              height: 20,
+            ),
+
+            Container(//Button
+             decoration: const BoxDecoration(
+                color: Colors.blueAccent,
+              ),
+              width: double.infinity,
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(vertical: 12),
+              child: const Text('LogIn')
+            )
+
+          ],
+        )
+      )
+    );
+  }
+}
